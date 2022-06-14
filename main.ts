096a58f401be5f9847dca242083bc2c07e817d3c
ThaCheeseBun <36112086+ThaCheeseBun@users.noreply.github.com>
2022-06-14 14:55:22 +0200

    dasdasda

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 // setup serial
-serial.setBaudRate(9600);
+serial.setBaudRate(115200);
 
 // settings
 const READDELAYMS = 10;
@@ -9,13 +9,16 @@ const CLK_PIN = DigitalPin.P13;
 const ATT_PIN = DigitalPin.P16;
 const MODE = ANALOGMODE;
 
-let lastData = [-1, -1, -1, -1];
+// vars
+let lastData;
 
 // init controller
 if (!initializeController()) {
-    serial.writeLine("nah it brokie");
+    //serial.writeLine("nah it brokie");
+    basic.showIcon(IconNames.Sad);
 } else {
-    serial.writeLine("init success :)");
+    //serial.writeLine("init success :)");
+    basic.showIcon(IconNames.Happy);
 
     // read inputs in a loop
     while (true) {
@@ -27,106 +30,10 @@ if (!initializeController()) {
             rawData[3] & btnChangedState[0],
             rawData[4] & btnChangedState[1],
         ];
-        if (JSON.stringify(lastData) != JSON.stringify(arr)) {
-            serial.writeNumber(~rawData[3] & btnChangedState[0]);
-            serial.writeString(".");
-            serial.writeNumber(~rawData[4] & btnChangedState[1]);
-            serial.writeString(".");
-            serial.writeNumber(rawData[3] & btnChangedState[0]);
-            serial.writeString(".");
-            serial.writeNumber(rawData[4] & btnChangedState[1]);
-            serial.writeString(",");
-            lastData = arr;
+        if (lastData != JSON.stringify(arr)) {
+            serial.writeString(arr.join(',') + '.');
+            lastData = JSON.stringify(arr);
         }
-
-        let btnDowns = [
-            ~rawData[3] & btnChangedState[0],
-            ~rawData[4] & btnChangedState[1],
-        ];
-        let btnUps = [
-            rawData[3] & btnChangedState[0],
-            rawData[4] & btnChangedState[1],
-        ];
-
-        /*if (CHK(btnDowns[0], BTN_SELECT))
-            serial.writeLine("BTN_SELECT has been pushed DOWN");
-        else if (CHK(btnUps[0], BTN_SELECT))
-            serial.writeLine("BTN_SELECT has been RELEASED");
-
-        if (CHK(btnDowns[0], BTN_START))
-            serial.writeLine("BTN_START has been pushed DOWN");
-        else if (CHK(btnUps[0], BTN_START))
-            serial.writeLine("BTN_START has been RELEASED");
-
-        if (CHK(btnDowns[0], BTN_LEFT_JOY))
-            serial.writeLine("BTN_LEFT_JOY has been pushed DOWN");
-        else if (CHK(btnUps[0], BTN_LEFT_JOY))
-            serial.writeLine("BTN_LEFT_JOY has been RELEASED");
-
-        if (CHK(btnDowns[0], BTN_RIGHT_JOY))
-            serial.writeLine("BTN_RIGHT_JOY has been pushed DOWN");
-        else if (CHK(btnUps[0], BTN_RIGHT_JOY))
-            serial.writeLine("BTN_RIGHT_JOY has been RELEASED");
-
-        if (CHK(btnDowns[0], BTN_UP))
-            serial.writeLine("BTN_UP has been pushed DOWN");
-        else if (CHK(btnUps[0], BTN_UP))
-            serial.writeLine("BTN_UP has been RELEASED");
-
-        if (CHK(btnDowns[0], BTN_RIGHT))
-            serial.writeLine("BTN_RIGHT has been pushed DOWN");
-        else if (CHK(btnUps[0], BTN_RIGHT))
-            serial.writeLine("BTN_RIGHT has been RELEASED");
-
-        if (CHK(btnDowns[0], BTN_DOWN))
-            serial.writeLine("BTN_DOWN has been pushed DOWN");
-        else if (CHK(btnUps[0], BTN_DOWN))
-            serial.writeLine("BTN_DOWN has been RELEASED");
-
-        if (CHK(btnDowns[0], BTN_LEFT))
-            serial.writeLine("BTN_LEFT has been pushed DOWN");
-        else if (CHK(btnUps[0], BTN_LEFT))
-            serial.writeLine("BTN_LEFT has been RELEASED");
-
-        if (CHK(btnDowns[1], BTN_L2))
-            serial.writeLine("BTN_L2 has been pushed DOWN");
-        else if (CHK(btnUps[1], BTN_L2))
-            serial.writeLine("BTN_L2 has been RELEASED");
-
-        if (CHK(btnDowns[1], BTN_R2))
-            serial.writeLine("BTN_R2 has been pushed DOWN");
-        else if (CHK(btnUps[1], BTN_R2))
-            serial.writeLine("BTN_R2 has been RELEASED");
-
-        if (CHK(btnDowns[1], BTN_L1))
-            serial.writeLine("BTN_L1 has been pushed DOWN");
-        else if (CHK(btnUps[1], BTN_L1))
-            serial.writeLine("BTN_L1 has been RELEASED");
-
-        if (CHK(btnDowns[1], BTN_R1))
-            serial.writeLine("BTN_R1 has been pushed DOWN");
-        else if (CHK(btnUps[1], BTN_R1))
-            serial.writeLine("BTN_R1 has been RELEASED");
-
-        if (CHK(btnDowns[1], BTN_TRIANGLE))
-            serial.writeLine("BTN_TRIANGLE has been pushed DOWN");
-        else if (CHK(btnUps[1], BTN_TRIANGLE))
-            serial.writeLine("BTN_TRIANGLE has been RELEASED");
-
-        if (CHK(btnDowns[1], BTN_CIRCLE))
-            serial.writeLine("BTN_CIRCLE has been pushed DOWN");
-        else if (CHK(btnUps[1], BTN_CIRCLE))
-            serial.writeLine("BTN_CIRCLE has been RELEASED");
-
-        if (CHK(btnDowns[1], BTN_CROSS))
-            serial.writeLine("BTN_CROSS has been pushed DOWN");
-        else if (CHK(btnUps[1], BTN_CROSS))
-            serial.writeLine("BTN_CROSS has been RELEASED");
-
-        if (CHK(btnDowns[1], BTN_SQUARE))
-            serial.writeLine("BTN_SQUARE has been pushed DOWN");
-        else if (CHK(btnUps[1], BTN_SQUARE))
-            serial.writeLine("BTN_SQUARE has been RELEASED");*/
 
         basic.pause(READDELAYMS);
     }
---
+++ old.ts
@@ -0,0 +1,88 @@
+/*let btnDowns = [
+            ~rawData[3] & btnChangedState[0],
+            ~rawData[4] & btnChangedState[1],
+        ];
+        let btnUps = [
+            rawData[3] & btnChangedState[0],
+            rawData[4] & btnChangedState[1],
+        ];*/
+
+/*if (CHK(btnDowns[0], BTN_SELECT))
+    serial.writeLine("BTN_SELECT has been pushed DOWN");
+else if (CHK(btnUps[0], BTN_SELECT))
+    serial.writeLine("BTN_SELECT has been RELEASED");
+
+if (CHK(btnDowns[0], BTN_START))
+    serial.writeLine("BTN_START has been pushed DOWN");
+else if (CHK(btnUps[0], BTN_START))
+    serial.writeLine("BTN_START has been RELEASED");
+
+if (CHK(btnDowns[0], BTN_LEFT_JOY))
+    serial.writeLine("BTN_LEFT_JOY has been pushed DOWN");
+else if (CHK(btnUps[0], BTN_LEFT_JOY))
+    serial.writeLine("BTN_LEFT_JOY has been RELEASED");
+
+if (CHK(btnDowns[0], BTN_RIGHT_JOY))
+    serial.writeLine("BTN_RIGHT_JOY has been pushed DOWN");
+else if (CHK(btnUps[0], BTN_RIGHT_JOY))
+    serial.writeLine("BTN_RIGHT_JOY has been RELEASED");
+
+if (CHK(btnDowns[0], BTN_UP))
+    serial.writeLine("BTN_UP has been pushed DOWN");
+else if (CHK(btnUps[0], BTN_UP))
+    serial.writeLine("BTN_UP has been RELEASED");
+
+if (CHK(btnDowns[0], BTN_RIGHT))
+    serial.writeLine("BTN_RIGHT has been pushed DOWN");
+else if (CHK(btnUps[0], BTN_RIGHT))
+    serial.writeLine("BTN_RIGHT has been RELEASED");
+
+if (CHK(btnDowns[0], BTN_DOWN))
+    serial.writeLine("BTN_DOWN has been pushed DOWN");
+else if (CHK(btnUps[0], BTN_DOWN))
+    serial.writeLine("BTN_DOWN has been RELEASED");
+
+if (CHK(btnDowns[0], BTN_LEFT))
+    serial.writeLine("BTN_LEFT has been pushed DOWN");
+else if (CHK(btnUps[0], BTN_LEFT))
+    serial.writeLine("BTN_LEFT has been RELEASED");
+
+if (CHK(btnDowns[1], BTN_L2))
+    serial.writeLine("BTN_L2 has been pushed DOWN");
+else if (CHK(btnUps[1], BTN_L2))
+    serial.writeLine("BTN_L2 has been RELEASED");
+
+if (CHK(btnDowns[1], BTN_R2))
+    serial.writeLine("BTN_R2 has been pushed DOWN");
+else if (CHK(btnUps[1], BTN_R2))
+    serial.writeLine("BTN_R2 has been RELEASED");
+
+if (CHK(btnDowns[1], BTN_L1))
+    serial.writeLine("BTN_L1 has been pushed DOWN");
+else if (CHK(btnUps[1], BTN_L1))
+    serial.writeLine("BTN_L1 has been RELEASED");
+
+if (CHK(btnDowns[1], BTN_R1))
+    serial.writeLine("BTN_R1 has been pushed DOWN");
+else if (CHK(btnUps[1], BTN_R1))
+    serial.writeLine("BTN_R1 has been RELEASED");
+
+if (CHK(btnDowns[1], BTN_TRIANGLE))
+    serial.writeLine("BTN_TRIANGLE has been pushed DOWN");
+else if (CHK(btnUps[1], BTN_TRIANGLE))
+    serial.writeLine("BTN_TRIANGLE has been RELEASED");
+
+if (CHK(btnDowns[1], BTN_CIRCLE))
+    serial.writeLine("BTN_CIRCLE has been pushed DOWN");
+else if (CHK(btnUps[1], BTN_CIRCLE))
+    serial.writeLine("BTN_CIRCLE has been RELEASED");
+
+if (CHK(btnDowns[1], BTN_CROSS))
+    serial.writeLine("BTN_CROSS has been pushed DOWN");
+else if (CHK(btnUps[1], BTN_CROSS))
+    serial.writeLine("BTN_CROSS has been RELEASED");
+
+if (CHK(btnDowns[1], BTN_SQUARE))
+    serial.writeLine("BTN_SQUARE has been pushed DOWN");
+else if (CHK(btnUps[1], BTN_SQUARE))
+    serial.writeLine("BTN_SQUARE has been RELEASED");*/
